d7ce173fb44bea6dd30867eb68a597d62a7128be
Angela <agarcia@grupomayo.com>
2025-07-09 11:43:46 +0200

    update metadata

--- src/components/molecules/navigation.tsx
+++ src/components/molecules/navigation.tsx
@@ -1,5 +1,11 @@
 export const Navigation = () => {
   return (
-    <div>Navigation</div>
+    <nav>
+      {/* Logo */}
+          <div className="flex-shrink-0">
+            <span className="text-2xl font-bold bg-gradient-to-r from-purple-600 via-violet-600 to-purple-500 bg-clip-text text-transparent">AG·DEV</span>
+          </div>
+
+    </nav>
   )
 }
